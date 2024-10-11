0697f662233b017f2a39e947007c7b85315448ce
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-10-11 15:23:31 +0300

    feat(week3): c# program

---
+++ BIB105-A/week3/lab/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicCodes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Dictionary<string, int> months = new Dictionary<string, int>();
+      months.Add("ocak", 30);
+      months.Add("subat", 28);
+      months.Add("mart", 30);
+      Console.WriteLine("Ay giriniz:");
+      string month = Console.ReadLine().ToLower().Trim();
+
+
+      Console.WriteLine("Ortalama m3 tuketim:");
+      double m3 = double.Parse(Console.ReadLine()) * months[month];
+
+      Console.WriteLine($"{month} ayın fature tutarı {m3}.");
+
+
+    }
+  }
+}
+
+
